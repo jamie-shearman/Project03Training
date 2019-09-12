8869d7a37715c4e3dbd4f64df73bbf0be3263ab0
Prashanth Puranik <puranik@digdeeper.in>
2019-09-12 19:46:33 +0530

    number 30 exercise

---
+++ session-demos/30-filtering-array-exercise.js
@@ -0,0 +1,16 @@
+// A - O
+// P - Z
+var city = 'New York'
+
+// strings can be compared using relational operators
+if( city < "P" ) {
+    console.log( city );
+    console.log( "Look in Volume 1")
+} else {
+    console.log( "Try again" );
+}
+
+newArray = []
+newArray = array[i]
+
+// s - z ; result will be [ ]
