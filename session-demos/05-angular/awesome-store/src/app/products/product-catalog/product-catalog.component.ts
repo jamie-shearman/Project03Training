cac606fabed560f5ab2312fae9529fecf76029ae
Prashanth Puranik <puranik@digdeeper.in>
2019-10-11 21:32:53 +0530

    till filtering

--- session-demos/05-angular/awesome-store/src/app/products/product-catalog/product-catalog.component.ts
+++ session-demos/05-angular/awesome-store/src/app/products/product-catalog/product-catalog.component.ts
@@ -12,13 +12,39 @@ import { ProductsService } from '../products.service';
 export class ProductCatalogComponent {
     title: string = 'Product Catalog';
     products: IProduct[];
+    filteredProducts: IProduct[];
+    displayHeading = false;
+    displayImages = true;
+    buttonText = 'Hide images';
+    filterKey = 'abc';
 
     constructor( private productsService : ProductsService ) {
         this.productsService.getProducts().subscribe(
             // success
             ( products : IProduct[] ) => {
                 this.products = products;
+                this.filteredProducts = this.products;
             }
         )
+    }
+
+    toggleImages() {
+        if( this.displayImages === true ) {
+            this.displayImages = false;
+            this.buttonText = 'Show images';
+        } else {
+            this.displayImages = true;
+            this.buttonText = 'Hide images';
+        }
+    }
+
+    filter() {
+        console.log( this.filterKey );
+
+        const selectProductsThatMatchFilterKey = ( product ) => {
+            return product.name.toUpperCase().indexOf( this.filterKey.toUpperCase() ) !== -1;
+        };
+
+        this.filteredProducts = this.products.filter( selectProductsThatMatchFilterKey );
     }
 }
--- session-demos/05-angular/awesome-store/src/app/products/products.module.ts
+++ session-demos/05-angular/awesome-store/src/app/products/products.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCatalogComponent } from './product-catalog/product-catalog.component';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ProductCatalogComponent],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
 })
 export class ProductsModule { }
